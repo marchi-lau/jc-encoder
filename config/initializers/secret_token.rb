1686d8444b33ccea21cf1998c98e479e7bffbf53
Marchi Lau <marchi.lau@gmail.com>
2011-07-18 20:25:39 +0800

    Initialize

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JcEncoder::Application.config.secret_token = '1b41494a50728fa418fd24461829305175e7bcc3d719ca675e95b8727d473eba0e9ff08f94377ff4d3024dc5d2ff0506692516ab3c9eaee0965e8cca36c0dc27'
