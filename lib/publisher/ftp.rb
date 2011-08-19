496f0aaafdbb83c029e358657589a1e0e99abe02
Marchi Lau <marchi.lau@gmail.com>
2011-08-19 11:20:27 +0800

    Bug Fixed - Upload Path.

--- lib/publisher/ftp.rb
+++ lib/publisher/ftp.rb
@@ -16,7 +16,12 @@ module Publisher
            system "curl --connect-timeout 20 --retry 20 -T '#{upload_txt}' --ftp-create-dirs -u #{ftp_username}:#{ftp_password} ftp://#{ftp_domain}#{destination}/upload.txt"
            
            # Upload Folder
-           destination = destination.chomp("/").slice(/.+\//).chomp("/").gsub(" ", '\\ ').chomp("/").slice(/.+\//).chomp("/").gsub(" ", '\\ ')
+           if destination.count("_") == 3
+             destination = destination.chomp("/").slice(/.+\//).chomp("/").gsub(" ", '\\ ').chomp("/").slice(/.+\//).chomp("/").gsub(" ", '\\ ')
+           else
+             destination = destination.chomp("/").slice(/.+\//).chomp("/").gsub(" ", '\\ ')
+           end
+             
            system  "/usr/local/bin/ncftpput -v -R -u #{ftp_username} -p #{ftp_password} #{ftp_domain} #{destination} '#{source}'"
            Notifier::Status("[FTP] #{ftp_domain} - Done", "#{source}")     
            
