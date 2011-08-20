b5bd54a60d9a0c1cdff6bfada1d7c9add5a7de42
Marchi Lau <marchi.lau@gmail.com>
2011-08-20 16:50:26 +0800

    Bug Fixed.

--- lib/publisher/ftp.rb
+++ lib/publisher/ftp.rb
@@ -10,36 +10,19 @@ module Publisher
         def upload(ftp_username, ftp_password, ftp_domain, source, destination)
            upload_txt = "#{Rails.root.to_s}/lib/publisher/upload.txt"
            File.open(upload_txt, 'w') {|f| f.write("") }
-           Notifier::Status("[FTP] #{ftp_domain} - Start", "#{source}")     
-
-           # Create Folder Tree
-           system "curl --connect-timeout 20 --retry 20 -T '#{upload_txt}' --ftp-create-dirs -u #{ftp_username}:#{ftp_password} ftp://#{ftp_domain}#{destination}/upload.txt"
-           
-           # Upload Folder
-           entries = Dir.entries(source)
-           entries.delete(".")
-           entries.delete("..")
-           
-           # entries.each do |file|
-           #   if File.directory?(file)
-           #     destination = destination.chomp("/").slice(/.+\//).chomp("/").gsub(" ", '\\ ').chomp("/").slice(/.+\//).chomp("/").gsub(" ", '\\ ')
-           #     puts "Has Directory"
-           #     break
-           #   else
-           #     destination = destination.chomp("/").slice(/.+\//).chomp("/").gsub(" ", '\\ ')
-           #     puts "No Directory"
-           #   end
-           # end
+           Notifier::Status("[FTP] #{ftp_domain} - Start", "#{source}")
+                
            language = destination.split("/").last
-           if language.include?("chi") or  language.include?("eng") or language.include?("pth")
+           if language.include?("chi") or language.include?("eng") or language.include?("pth")
              destination = destination.chomp("/").slice(/.+\//).chomp("/").gsub(" ", '\\ ').chomp("/").slice(/.+\//).chomp("/").gsub(" ", '\\ ')
            else
              destination = destination.chomp("/").slice(/.+\//).chomp("/").gsub(" ", '\\ ')
            end
-            
-            puts destination
-            
            
+           
+           # Create Folder Tree
+           system "curl --connect-timeout 20 --retry 20 -T '#{upload_txt}' --ftp-create-dirs -u #{ftp_username}:#{ftp_password} ftp://#{ftp_domain}#{destination}/upload.txt"
+                 
            system  "/usr/local/bin/ncftpput -v -R -u #{ftp_username} -p #{ftp_password} #{ftp_domain} #{destination} '#{source}'"
            Notifier::Status("[FTP] #{ftp_domain} - Done", "#{source}")     
            
